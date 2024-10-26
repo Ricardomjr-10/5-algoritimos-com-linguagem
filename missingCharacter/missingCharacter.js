5c1b8fc37662991acd7d855aad9eb385ef3a16a2
Ricardo Rocha <ricardomjr2@gmail.com>
2024-10-25 21:50:03 -0300

    frase modificada

---
+++ missingCharacter/missingCharacter.js
@@ -0,0 +1,13 @@
+function showLetters (frase) {
+    const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    const fraseArray = [...frase]
+    let letters = ''
+    for (let i = 0; i < alphabet.length;i++) {
+        if (!fraseArray.includes(alphabet[i])) {
+            letters += ` ${alphabet[i]}`
+        }
+    }
+    console.log(letters)
+}
+const frase = 'o rato roeu a roupa do rei de roman na bahia'
+showLetters(frase)
